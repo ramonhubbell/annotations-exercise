672068444a69784a6ce5ba3fdbd7468eb0ad5bd3
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-05-14 14:40:48 -0500

    finished first part of exercise

--- src/Author.java
+++ src/Author.java
@@ -3,7 +3,7 @@
  */
 import java.util.ArrayList;
 import java.util.List;
-
+@SuppressWarnings({"decprecation", "unchecked"})
 public class Author extends Person {
     private List books;
 
@@ -15,6 +15,7 @@ public class Author extends Person {
     /**
      * @deprecated Use publishedBooks instead
      */
+
     @Deprecated
     public List<String> getBooks() {
         return books;
@@ -28,7 +29,7 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Override
+//    @Override
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
