807ed6a5a9490a4bf401d444df9162208dd6cf1f
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-05-14 21:39:03 -0500

    finished annotation exercise

--- src/Author.java
+++ src/Author.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
@@ -28,8 +28,8 @@ public class Author extends Person {
         books.add(book);
     }
 
-//    @Override
-    public String sortName() {
+    @Override
+    public String fullName() {
         return String.format("%s, %s", lastName, firstName);
     }
 }
